b8964f2e15aca40422f439c1873dc1d09f535c30
Pushpavathi4 <pes1202203228@pesu.pes.edu>
2025-12-16 13:24:04 +0530

    Create solution35.py

---
+++ solution35.py
@@ -0,0 +1,10 @@
+#Coding Challenge 35: Printing * Increasing Pattern (N Rows)
+n=int(input("Enter n:"))
+if(n<=0):
+    print("Number should be a positive interger which is greater than 0.")
+else:
+    for i in range(1,n+1):
+        ans=""
+        for j in range(0,i):
+           ans+='*'
+        print(ans)
